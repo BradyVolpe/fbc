fe1fc02804cf604a01f74fb61da16a9bf10f011d
Brady Volpe <brady.volpe@nimble-this.com>
2022-01-06 14:56:38 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# FBC or Full Band Capture
+This Python appliction configures enables a cable modem for Full Band Capture (FBC). FBC captures all RF signals on the input of a cable modem
+
+## Overview
+This document contains more than just documentation of FBC. Scripts the author has been writing in Perl are now being migrated to Python. Some work has been done to ensure continuity on constomer servers and miminmal impact. For that purpose, only virtual envirmonments will be used when deploying and testing with Python scripts moving forward. This will be required. As part of this, documementaiton for virtual environments (venv) will be part of the README.md files in each repo where such files are provided, such as this, along with theh respective requirements.txt file.
+
+## Dependencie
+
+### CentOS / RHEL 7
+
+    sudo yum install net-snmp-devel
+    sudo yum install python-devel
+    sudo yum install python3-devel
+
+### Mac OS:
+
+Ensure you have the correct version of NET-SNMP:
+    $ snmpget --version
+    NET-SNMP version: 5.6.2.1
+    EasySNMP requires (I believe) NET-SNMP v5.7 or higher.
+
+After running brew install net-snmp, this updated the version to the following:
+
+    snmpget --version
+    NET-SNMP version: 5.9.1
+
+Install Python3 for Mac:
+    https://www.python.org/downloads/
+
+Install requirements for Virtual environment:
+    
+
+## Virtual Environment and Running the Code
+### Linux
+    unset PYTHONPATH
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt
+    python e6k_utsc.py <IPv4 : 1, IPv6 2> <CMTS IP> <CMTS RW community string> <Upstream ifIndex> <ssh user> <ssh pw>
+### Mac OS:
+    python3 -m venv .venv
+	source .venv/bin/activate
+    pip install -r requirements.txt
+    python e6k_utsc.py <IPv4 : 1, IPv6 2> <CMTS IP> <CMTS RW community string> <Upstream ifIndex> <ssh user> <ssh pw>
+## Troubleshooting
+
+### CentOS / RHEL 7 
+
+Issues with Packages in Virtenv
+Run:
+    printenv
+
+If PYTHONPATH=/usr/lib/python2.7/site-packages/ —> is there then run:
+	unset PYTHONPATH
+
+### Mac OS
+
+If you don't have the correct version of NET-SNMP you will get the following error when trying to install easysnmp:
+
+    from easysnmp import Session
+    session = Session(hostname='localhost', community='public', version=2)
+
+This results in an error message as follows:
+  
+    $ python test.py 
+    Traceback (most recent call last):
+    File "/Users/me/python_utsc/test.py", line 9, in <module>
+    session = Session(hostname='localhost', community='public', version=2)
+    File "/Users/me/python_utsc/.venv/lib/python3.9/site-packages/easysnmp/session.py", line 280, in   __init__
+    self.sess_ptr = interface.session(
+    NameError: name 'interface' is not defined
+
+This occurs because easysnnmp requires NET-SNMP version 5.7 or higher. You can test as follows:
+
+    $ snmpget --version
+    NET-SNMP version: 5.6.2.1
+
+To upgrade on Mac, just run:
+    
+    brew install net-snmp
+
+Then your version of NET-SNMP should 5.7 or higher after checking:
+
+    snmpget --version
+    NET-SNMP version: 5.9.1
---
+++ getAndPlotFbcData.py
@@ -0,0 +1,233 @@
+#!/usr/bin/python3
+# Author: Brady Volpe
+# Date: September 29, 2020
+# Updated Aug, 2021
+# Nimble This LLC 2013
+# All Rights Reserved
+# No part of this website or any of its contents may be reproduced, copied, modified or adapted,
+# without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+#
+#
+# Collects docsIf3CmSpectrumAnalysisMeasAmplitudeData from cable modem and save to
+# CSV file along with saving the plotted graph file in the .html format
+#
+#
+# IMP : plotly, pysnmp and pandas modules are required for this script
+# USAGE: getAndPlotFbcData.py <IPv4 : 1, IPv6 2> <SNMP community string> <cable modem IP address> <filename>
+
+import csv
+from os import times
+import sys
+import time
+
+import plotly.express as px
+from pysnmp.hlapi import *
+
+# Define FBC SNMP OIDs used
+docsIf3CmSpectrumAnalysisMeasAmplitudeData = ".1.3.6.1.4.1.4491.2.1.20.1.35.1.2"
+
+# OID for checking modem reachability
+modemDescOID = "1.3.6.1.2.1.1.1.0"
+
+# Spectrum parameter OIDs
+enableAnalyzerOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.1.0"
+inactivityTimeoutOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.2.0"
+startFreqOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.3.0"
+stopFreqOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.4.0"
+spanOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.5.0"
+binOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.6.0"
+windowOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.8.0"
+avgOID = ".1.3.6.1.4.1.4491.2.1.20.1.34.9.0"
+
+# Convert 2's complement 16 hex value to decimal
+def twos(val):
+    x = int(val, 16)
+    if x > 32767:
+        x = x - 65535
+    return x
+
+# Select transport target based on the IpAddress type
+def getTransportTarget():
+    if(int(sys.argv[1]) == 1):
+        return UdpTransportTarget((str(sys.argv[3]), 161))
+    else:
+        return Udp6TransportTarget((str(sys.argv[3]), 161))
+
+
+def getFormattedTime(hr, min, sec):
+    return str(hr) + "hr:" + str(min) + "min:" + str(sec) + "sec"
+
+
+def isModemReachable():
+    for (errorIndication, errorStatus, errorIndex,
+         varBinds) in getCmd(SnmpEngine(),
+                             CommunityData(str(sys.argv[2])),
+                             getTransportTarget(),
+                             ContextData(),
+                             ObjectType(ObjectIdentity(
+                                 modemDescOID))):
+
+        if errorIndication:
+            print(errorIndication)
+            break
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex)-1][0] or '?'))
+            break
+        else:
+            print("Modem is reachable...")
+            print()
+
+
+def doSetSNMP(message, oid, value, typeFlag):
+    print(message)
+    if(typeFlag == "int"):
+        value = Integer(value)
+    elif(typeFlag == "gague32"):
+        value = Gauge32(value)
+    for (errorIndication, errorStatus, errorIndex,
+         varBinds) in setCmd(SnmpEngine(),
+                             CommunityData(str(sys.argv[2])),
+                             getTransportTarget(),
+                             ContextData(),
+                             ObjectType(ObjectIdentity(
+                                 oid), value)):
+
+        if errorIndication:
+            print(errorIndication)
+            break
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex)-1][0] or '?'))
+            break
+        else:
+            print("Value applied sucessfully")
+            print()
+
+
+def setSpectrum():
+    doSetSNMP("Enabling analyzer...", enableAnalyzerOID, 1, "int")
+    doSetSNMP("Setting inactivity timeout...",
+              inactivityTimeoutOID, 3000, "int")
+    doSetSNMP("Setting start freq...", startFreqOID, 93000000, "gague32")
+    doSetSNMP("Setting stop freq...", stopFreqOID, 993000000, "gague32")
+    doSetSNMP("Setting span...", spanOID, 7500000, "gague32")
+    doSetSNMP("Setting bin...", binOID, 256, "gague32")
+    doSetSNMP("Setting window...", windowOID, 1, "int")
+    doSetSNMP("Setting averge...", avgOID, 8, "gague32")
+
+
+def getFbcData():
+    ampdata = []
+    print("Getting FBC data...")
+    time.sleep(10)
+    startTime = time.time()
+    endLocalTime = time.localtime(startTime + 60)
+    while(time.time() <= (startTime + 60)):
+        currentTime = time.localtime()
+        print("Trying to get FBC data at ", getFormattedTime(currentTime.tm_hour, currentTime.tm_min, currentTime.tm_sec),
+              " last try at ", getFormattedTime(endLocalTime.tm_hour, endLocalTime.tm_min, endLocalTime.tm_sec))
+        errorOccurred = False
+        for (errorIndication, errorStatus, errorIndex,
+             varBinds) in bulkCmd(SnmpEngine(),
+                                  CommunityData(str(sys.argv[2])),
+                                  getTransportTarget(),
+                                  ContextData(),
+                                  0, 2,
+                                  ObjectType(ObjectIdentity(
+                                      docsIf3CmSpectrumAnalysisMeasAmplitudeData)),
+                                  lexicographicMode=False):
+
+            if errorIndication:
+                print(errorIndication)
+                errorOccurred = True
+                break
+            elif errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                                    errorIndex and varBinds[int(errorIndex)-1][0] or '?'))
+                errorOccurred = True
+                break
+            else:
+                for varBind in varBinds:
+                    ampdata.append(varBind[1].prettyPrint()[2:])
+        if len(ampdata) > 1 and errorOccurred == False:
+            break
+    doSetSNMP("Disabling analyzer...", enableAnalyzerOID, 2, "int")
+    if len(ampdata) <= 1:
+        print("No FBC data available")
+        sys.exit()
+    else:
+        print("Got the FBC data...")
+
+    # Decode each AmplitudeData row and write frequency,amplitude values to output file
+    try:
+        with open(sys.argv[4], "w") as outF:
+            ampDataLen = len(ampdata)
+            index = 0
+            for x in ampdata:
+                if(index < ampDataLen-1):
+                    centerfreq = int(x[0:8], 16)
+                    freqspan = int(x[8:16], 16)
+                    numbins = int(x[16:24], 16)
+                    binspacing = int(x[24:32], 16)
+                    resbw = int(x[32:40], 16)
+
+                    startfreq = centerfreq - (numbins / 2 * binspacing)
+
+                    i = 0
+                    bin = 0
+                    while i < numbins*4:
+                        freq = startfreq + (bin * binspacing)
+                        j = 40 + i
+                        hexvalue = x[j:j+4]
+                        ampvalue = twos(hexvalue) / 100.0
+
+                        print(str(freq) + "," + str(ampvalue), file=outF)
+
+                        i += 4
+                        bin += 1
+                    index += 1
+    except Exception as ex:
+        print("Error occurred while processing amplitude data : ", ex)
+        sys.exit()
+
+
+
+def showFbcData():
+    print("Plotting graph...")
+    xdata = []
+    ydata = []
+
+    with open(sys.argv[4]) as csvfile:
+        csvData = csv.reader(csvfile, delimiter=',')
+        for row in csvData:
+            xdata.append(float(row[0]) / 1000000)
+            ydata.append(float(row[1]))
+
+    fig = px.line(x=xdata, y=ydata,
+                  labels={'x': 'Frequency (MHz)', 'y': 'Amplitude (dB)'},
+                  title='DOCSIS Cable Modem Full Band Capture', markers=True)
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+                      title={'x': 0.5, 'xanchor': 'center'})
+    fig.update_traces(hovertemplate='Frequency: <b>%{x:.2f} MHz</b><br>Amplitude: <b>%{y:.2f} dB</b>',
+                      line_color='rgb(68,114,196)')
+    fig.update_xaxes(showline=True, linewidth=2,
+                     linecolor='black', gridcolor='rgb(209,209,209)')
+    fig.update_yaxes(showline=True, linewidth=2,
+                     linecolor='black', gridcolor='rgb(209,209,209)')
+    fig.write_html(sys.argv[4] + ".html")
+    print("Graph saved into ", str(sys.argv[4] + ".html"))
+
+
+if __name__ == "__main__":
+    
+    if ((len(sys.argv)) < 5):
+        print("Usage: getAndPlotFbcData.py <IPv4 : 1, IPv6 2> <SNMP community string> <cable modem IP address> <filename>")
+        sys.exit()
+
+    isModemReachable()
+    setSpectrum()
+    getFbcData()
+    showFbcData()
+
+# End of file
